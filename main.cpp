151f302bb235ea5f718e832d0652739bd06d6a60
Aleksandar Atanasov <redbaronqueen@gmail.com>
2016-05-31 22:01:44 +0200

    Added binaries (due to old compiler of another computer where C++14 is not available

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include "prioritymap.hpp"
 #include <iostream>
 
-// TODO Add modes and override run(); using static_cast or dynamic_cast add different Mode-types to the map
 // TODO Add mode execution class
 // TODO Clean properly - also change normal pointers to boost::smart pointers
 // TODO Use signals triggered from Mode to map to tell map that a mode execution has finished or timedout in which case it has to be deleted from map (if last mode in a PriorityGroup delete whole group)
